87f91605884f9fdea9e74930556174e5b133e588
ramolo <71272744+ramolo@users.noreply.github.com>
2020-09-15 23:32:20 +0200

    continuacion ejercicio2
    continuacion ejercicio2

--- ejercicio2.php
+++ ejercicio2.php
@@ -18,11 +18,21 @@
 	echo "<form><input type='radio' name='sexo' value='hombre'></form> HoMbrE";
 	echo "<form><input type='radio' name='sexo' value='mujer'></form> MuJeR</br></br>";
 
-	echo "<form><input type='submit' name='busqueda' value='Buscar'</br></br></br>";
+	echo "<form><input type='submit' name='busqueda' value='Buscar'</form></br></br></br>";
 
-	echo "<form><input type='reset' name='resetear' value='Resetear formulario'";
+	echo "<form><input type='reset' name='resetear' value='Resetear formulario'</form></br></br></br>";
 
-	echo "<form><input type='"
+	//no hace falta cerrar la etiqueta del formulario para que lo muestre?
+
+	echo "<form><input type='file' name='insertarAdjunto'</form></br></br></br>";
+
+	echo "<form><input type='image' name='insertearImagen' value='Enviamos la imagen?'</form></br></br></br>";
+
+	echo "<form><input type='hidden' name='urlPrevia' value='articulo/primero.html'</form>"; //?¿?¿campo oculto para añadir info oculta en el formulario...?
+
+	echo "<form><input type='button' name='guardar' value='Guardar cambios'</form>(si la página contiene javascript, estos botones se pueden programar para realizar cualquier tarea compleja cuando se pulsa sobre ellos)</form>";
+
+
 
 
 
