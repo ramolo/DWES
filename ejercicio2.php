0dcabcba77f31377388c67a19d3ba213380c6590
ramolo <71272744+ramolo@users.noreply.github.com>
2020-09-15 20:55:28 +0200

    comienzo ejercicio 2
    ejercicio2 sin acabar, practicados algo mas de la mitad de los formularios

---
+++ ejercicio2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	
+<?php
+	echo "Escribe tu nombre";
+	echo "<form><input type ='text' name='nombre' value=''></form></br>";
+
+	echo "Escribe tu contraseña";
+	echo "<form><input type ='password' name='contraseña' value=''></form></br>";
+
+	echo "casilla de chequeo";
+	echo "<form><input type ='checkbox' name='verificacion' value=''></form></br>";
+
+	echo "<form><input type='radio' name='sexo' value='hombre'></form> HoMbrE";
+	echo "<form><input type='radio' name='sexo' value='mujer'></form> MuJeR</br></br>";
+
+	echo "<form><input type='submit' name='busqueda' value='Buscar'</br></br></br>";
+
+	echo "<form><input type='reset' name='resetear' value='Resetear formulario'";
+
+	echo "<form><input type='"
+
+
+
+?>
+	
+</body>
+</html>
